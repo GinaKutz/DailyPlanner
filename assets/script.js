82ce55ed05d3a7b7b15f93a2efa9182d7014cd80
GinaKutz <gjkutz@gmail.com>
2023-12-28 11:35:52 -0600

    update on html and js

--- assets/script.js
+++ assets/script.js
@@ -20,4 +20,48 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+
+    
+        // Click event listener for the save button
+        $('.saveBtn').on('click', function() {
+          const description = $(this).siblings('.description').val();
+          const blockID = $(this).closest('.time-block').attr('id');
+      
+          localStorage.setItem(blockID, description);
+        });
+      
+        // Function to update time block classes
+        function updateTimeBlocks() {
+          const currentHour = new Date().getHours();
+      
+          $('.time-block').each(function() {
+            const blockHour = parseInt($(this).attr('id').split('-')[1]);
+      
+            if (blockHour < currentHour) {
+              $(this).removeClass('present future').addClass('past');
+            } else if (blockHour === currentHour) {
+              $(this).removeClass('past future').addClass('present');
+            } else {
+              $(this).removeClass('past present').addClass('future');
+            }
+          });
+        }
+      
+        updateTimeBlocks(); // Initial update
+      
+        // Retrieve user inputs from localStorage and set textarea values
+        $('.time-block').each(function() {
+          const blockID = $(this).attr('id');
+          const savedDescription = localStorage.getItem(blockID);
+      
+          if (savedDescription !== null) {
+            $(this).find('.description').val(savedDescription);
+          }
+        });
+      
+        // Display current date in the header
+        const currentDate = new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+        $('#current-date').text(currentDate); 
+    
+      
   });
